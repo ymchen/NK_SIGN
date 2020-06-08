3698968e24fd7c56222e4f55115a3ec5e09d0621
ChenYiMin <loseyyou@gmail.com>
2020-06-08 16:46:40 +0800

    nk_sign_v1.py add by chenym 20200608

--- CYM_TOOLS/DataBaseTool.py
+++ CYM_TOOLS/DataBaseTool.py
@@ -3,14 +3,14 @@
 # @Email:   254906610@qq.com
 # @Date:   2019-12-16 15:40:40
 # @Last Modified by:   chenym
-# @Last Modified time: 2020-06-05 14:24:22
+# @Last Modified time: 2020-06-08 16:27:19
 
 import pymysql
 
 class DataBaseTools():
     def __init__(self):
-        #self.con = pymysql.connect("49.235.122.74","carry","carry","krcdb")
-        self.con = pymysql.connect("localhost", "root", "root", "qqdb")
+        self.con = pymysql.connect("49.235.122.74","carry","carry","krcdb")
+        #self.con = pymysql.connect("localhost", "root", "root", "qqdb")
         self.cursor = self.con.cursor()
     def query(self,sql):
         print("[Qurey_Sql Info]："+sql)
@@ -41,20 +41,21 @@ class DataBaseTools():
         except:
             self.con.rollback()
             self.cursor.close()
-    def get_InsertSql(self,List):
+    def get_InsertSql(self,tableNm,List):
         dataTuple = tuple(List)
         dataStr = str(dataTuple)
         colList = []
         for v_col in range(1,len(List)+1):
             colList.append("col"+str(v_col))
         coldata = str(tuple(colList)).replace("'",'')
-        v_Intsql = "Insert into  kaoqin  "+coldata+" values "+dataStr+";"
+        v_Intsql = "Insert into  "+tableNm+coldata+" values "+dataStr+";"
         return  v_Intsql
 if __name__ == '__main__':
     db = DataBaseTools()
     alist = ["chenyimin2","password"]
-    print(db.get_InsertSql(alist))
-    print(db.DeleteSql(" truncate table  kaoqin; "));
-    print(db.query("select * from kaoqin limit 11"));
+    print(db.get_InsertSql("nk_sign_d_info",alist))
+    print(db.Ins(db.get_InsertSql("nk_sign_d_info",alist)))
+    #print(db.DeleteSql(" truncate table  nk_sign_d_info; "));
+    #print(db.query("select * from nk_sign_d_info limit 11"));
 
 # select date(rec_crt_dt1)  from kaoqin  where  date(rec_crt_dt1) = date('2020-06-05')
--- NK_SIGN_V1.py
+++ NK_SIGN_V1.py
@@ -3,16 +3,18 @@
 # @Email:	254906610@qq.com
 # @Date:   2020-06-04 14:39:31
 # @Last Modified by:   chenym
-# @Last Modified time: 2020-06-08 15:48:38
+# @Last Modified time: 2020-06-08 16:44:05
 import datetime
 import re
 import time
 from   CYM_TOOLS.WebTool import Webtools
 from   CYM_TOOLS.MapTool import Maptools
 from   CYM_TOOLS.UnvsTool import Unvstools
+from   CYM_TOOLS.DataBaseTool import DataBaseTools
 unvstool = Unvstools()
 maptool = Maptools('4d7e070d9ca8a48f2894c852eed08f74',600)
 webtool = Webtools()
+db = DataBaseTools()
 month = unvstool.getCurMon()
 punchCardDate = unvstool.getCurDate()
 file = "NorthKingUser.ini"
@@ -40,16 +42,28 @@ def signMonRep(user_name):
     employeeInfos = logon_json['RSP_BODY']['employeeInfos']
     return employeeInfos
 def signRepNow(user_name):
+    sign_info_list =[]
+    i = 0
     logon_url = 'http://111.203.253.37:8201/nkcisp/mobile-base.action?employeeNo='+user_name+'&to=cispQueryEmployeeByProjectInfoAction'
     logon_info = webtool.web_Get(logon_url).text
     logon_json = unvstool.toJson(logon_info)
+    employeeName = logon_json['RSP_BODY']['projectList'][0]['employeeName']
     projectNo = logon_json['RSP_BODY']['projectList'][0]['projectNo']
     logon_url = 'http://111.203.253.37:8201/nkcisp/mobile-base.action?employeeId='+user_name+'&projectNo='+projectNo+'&punchCardDate='+punchCardDate+'&to=cispQueryNormalCardListAction'
     logon_info = webtool.web_Get(logon_url).text
     logon_json = unvstool.toJson(logon_info)
+    employeeId = logon_json['RSP_BODY']['employeeId']
     cardList = logon_json['RSP_BODY']['cardList']
-    return cardList
-
+    for var_rec in cardList:
+        var_list  = [i]
+        var_list.append(employeeId)
+        var_list.append(employeeName)
+        var_list.append(punchCardDate)
+        var_list.append(var_rec['punchCardTime'])
+        var_list.append(var_rec['punchPlace'])
+        i = i+1
+        sign_info_list.append(var_list)
+    return sign_info_list
 def sign(user_name):
     logon_url = 'http://111.203.253.37:8201/nkcisp/mobile-base.action?employeeNo='+user_name+'&to=cispQueryEmployeeByProjectInfoAction'
     logon_info = webtool.web_Get(logon_url).text
@@ -81,15 +95,18 @@ def sign(user_name):
 userList = unvstool.getDataKeyValue('NorthingList',"userList")
 userArr = userList.split(",")
 # 循环处理身份信息
+delSql = "Delete from nk_sign_d_info  where  date(rec_crt_dt1) = date('"+punchCardDate+"')"
+print(db.DeleteSql(delSql));
 for var_user in userArr:
     # 打卡
     #sign(var_user)
     # 查看当月打卡情况    
     #print(signMonRep(var_user))
-
     # 查看当天打卡情况
-    print(var_user)
+    #print(var_user)
     for var_rec in signRepNow(var_user):
-        print(var_rec['punchCardTime']+' '+var_rec['punchPlace'])
-
+        Insql = db.get_InsertSql("nk_sign_d_info",var_rec)
+        print(db.Ins(Insql))
+    #print(db.DeleteSql(" truncate table  nk_sign_d_info; "));
+    #print(db.query("select * from nk_sign_d_info limit 11"));
 
